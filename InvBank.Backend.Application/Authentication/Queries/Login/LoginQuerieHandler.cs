5e972a39241072f5250e98a38f3ad8bf7f3609c3
CavasCallahan <dassuncao1234@gmail.com>
2023-06-22 12:37:39 +0100

    add: Validation to LoginQuerie

--- InvBank.Backend.Application/Authentication/Queries/Login/LoginQuerieHandler.cs
+++ InvBank.Backend.Application/Authentication/Queries/Login/LoginQuerieHandler.cs
@@ -1,6 +1,8 @@
 using ErrorOr;
+using FluentValidation;
 using InvBank.Backend.Application.Common.Interfaces;
 using InvBank.Backend.Application.Common.Providers;
+using InvBank.Backend.Application.Services;
 using InvBank.Backend.Contracts.Authentication;
 using InvBank.Backend.Domain.Entities;
 using InvBank.Backend.Domain.Errors;
@@ -8,21 +10,34 @@ using MediatR;
 
 namespace InvBank.Backend.Application.Authentication.Queries.Login;
 
-public class LoginQuerieHandler : IRequestHandler<LoginQuerie, ErrorOr<AuthenticationResult>>
+public class LoginQuerieHandler : BaseService, IRequestHandler<LoginQuerie, ErrorOr<AuthenticationResult>>
 {
+    private readonly IValidator<LoginQuerie> _validator;
     private readonly IPasswordEncoder _passwordEncoder;
     private readonly IJWTModifier _jwtModifier;
     private readonly IUserRepository _userRepository;
 
-    public LoginQuerieHandler(IUserRepository userRepository, IJWTModifier jwtModifier, IPasswordEncoder passwordEncoder)
+    public LoginQuerieHandler(
+        IUserRepository userRepository,
+        IJWTModifier jwtModifier,
+        IPasswordEncoder passwordEncoder,
+        IValidator<LoginQuerie> validator)
     {
         _userRepository = userRepository;
         _jwtModifier = jwtModifier;
         _passwordEncoder = passwordEncoder;
+        _validator = validator;
     }
 
     public async Task<ErrorOr<AuthenticationResult>> Handle(LoginQuerie request, CancellationToken cancellationToken)
     {
+
+        var validationResult = await Validate<LoginQuerie>(_validator, request);
+
+        if (validationResult.IsError)
+        {
+            return validationResult.Errors;
+        }
 
         // Verify user
         Auth? userAuth = await _userRepository.GetUserAuth(request.Email);
